fd4e843f7fc89346312a8007e39053d9d454938f
James Turk <james.p.turk@gmail.com>
2003-01-15 05:38:57 +0000

    added file, from ZE_Error.h and ZE_Macros.h

---
+++ src/ZE_Utility.cpp
@@ -0,0 +1,38 @@
+/*******************************************************************************
+        This file is Part of the ZEngine Library for 2D game development.
+                   Copyright (C) 2002, 2003 James Turk
+
+                     Licensed under a BSD-style license.
+
+    The maintainer of this library is James Turk (james@conceptofzero.net) 
+     and the home of this Library is http://www.zengine.sourceforge.net
+*******************************************************************************/
+
+/*!
+\par File Header:
+File: ZE_Utility.cpp <br>
+Description: Implementation source file for ZEngine Utilities. <br>
+Author(s): James Turk <br>
+$Id: ZE_Utility.cpp,v 1.1 2003/01/15 05:38:57 cozman Exp $<br>
+
+    \file ZE_ZUtility.cpp
+    \brief Source file for ZEngine utility functions.
+
+    Source file containing open utilities for use inside and alongside ZEngine.
+**/
+
+#include "ZE_Utility.h"
+
+namespace ZE
+{
+
+string FormatStr(const char *fmtstr, ...)
+{
+    char buf[512];
+    va_list args;
+
+    va_start(args,fmtstr);
+    vsprintf(buf, fmtstr, args);
+    va_end(args);
+    return buf;
+}
