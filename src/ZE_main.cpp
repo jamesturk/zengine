f03de195d023b9d93bb403807ada5c1efc035af3
James Turk <james.p.turk@gmail.com>
2003-09-22 23:49:22 +0000

    documented ZE_main.cpp

--- src/ZE_main.cpp
+++ src/ZE_main.cpp
@@ -15,14 +15,28 @@
     The entry point 'main' defined in this file simply does the initial creation and freeing of ZEngine.
     An application that defines main overrides this main, so backwards compatibility is not broken, however as of 0.8.5 ZE_main is the 
     expected entry point of ZEngine applications.
-    <br>$Id: ZE_main.cpp,v 1.1 2003/09/09 02:45:58 cozman Exp $<br>
+    <br>$Id: ZE_main.cpp,v 1.2 2003/09/22 23:49:22 cozman Exp $<br>
     \author James Turk
 **/
 
 #include "ZEngine.h"
 
+/*!
+    \brief ZEngine entry point.
+    Preferred entry point for ZEngine programs, when using the ZE_main instead of main
+    there is no need to call the old InitPhysFS nor ZEngine::ReleaseInstance.
+    \param argc number of arguments 
+    \param argv argument array
+**/
 extern int ZE_main(int argc, char *argv[]);
 
+
+/*!
+    \brief Program entry point.
+    Predefined main which does needed initialization and then calls ZE_main, making it the ZEngine entry point.
+    \param argc number of arguments 
+    \param argv argument array
+**/
 int main(int argc, char *argv[])
 {
     ZE::ZEngine *engine = ZE::ZEngine::GetInstance();
